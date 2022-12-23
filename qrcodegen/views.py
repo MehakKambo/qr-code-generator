c40276c932c489552f192135caf99970eead6d16
MehakKambo <mehakpreetp12@gmail.com>
2022-12-23 12:06:52 -0800

    Added finishing touches

--- qrcodegen/views.py
+++ qrcodegen/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.conf import settings
+from django.http import HttpResponse
 from qrcode import *
 import time
+from io import BytesIO
 
 # Create your views here.
 
 def qr_gen(request):
     if request.method == "POST":
-        data = request.POST('data')
+        data = request.POST['data']
+        img = make(data)
+        response = HttpResponse(content_type='image/png')
+        response['Content-Disposition'] = 'attachment; filename="output.png"'
+        img.save(response,"PNG")
+        return response
+    return render(request, 'index.html')
