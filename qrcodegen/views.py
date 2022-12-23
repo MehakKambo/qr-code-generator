d5892b7958ddfdd8d5612b5c11d24fd46a8f5f94
MehakKambo <mehakpreetp12@gmail.com>
2022-12-23 11:21:47 -0800

    Setup the view

--- qrcodegen/views.py
+++ qrcodegen/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.conf import settings
+from qrcode import *
+import time
 
 # Create your views here.
+
+def qr_gen(request):
+    if request.method == "POST":
+        data = request.POST('data')
